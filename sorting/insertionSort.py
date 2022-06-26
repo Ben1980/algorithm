5fb7dd8c8a0fd295eff69cc24657610b776a49c3
Benjamin Mahr <ben.mahr@gmail.com>
2022-06-26 16:09:20 +0200

    error fixed an simplifyed

--- sorting/insertionSort.py
+++ sorting/insertionSort.py
@@ -7,9 +7,7 @@ def InsertionSort(A):
         key = A[j]
         i = j - 1
 
-        while i > 0 and A[i] > key:
+        while i >= 0 and A[i] > key:
             A[i + 1] = A[i]
             i = i - 1
         A[i + 1] = key
-    
-    return A
